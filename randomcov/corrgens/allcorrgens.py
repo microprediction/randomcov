22def32f65d65243da58a6cb396e687cef4e7e61
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2024-10-16 13:15:54 -0400

    Update allcorrgens.py

--- randomcov/corrgens/allcorrgens.py
+++ randomcov/corrgens/allcorrgens.py
@@ -2,5 +2,6 @@ from randomcov.corrgens.lkj import lkj_corr
 from randomcov.corrgens.wishart import wishart_corr
 from randomcov.corrgens.residuals import residuals_corr
 from randomcov.corrgens.walk import walk_corr
+from randomcov.corrgens.animals import animals_corr
 
 CORR_GENERATORS = [lkj_corr, wishart_corr, residuals_corr, walk_corr]
